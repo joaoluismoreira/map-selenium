d7941bd9aaa1a8b0ed91a764ab103aa915e5bb75
João Moreira <joao.moreira@xing.com>
2021-01-12 19:13:21 +0000

    pom implemented and test is green

--- src/main/java/pages/GoogleMapsPage.java
+++ src/main/java/pages/GoogleMapsPage.java
@@ -1,24 +1,43 @@
 package pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class GoogleMapsPage {
-    private WebDriver driver;
 
     //Page URL
+    private WebDriver driver;
     private static String PAGE_URL="https://www.google.com/maps";
+    private WebDriverWait wait;
+
 
     //Locators
     @FindBy(id = "searchboxinput")
     WebElement searchBox;
 
+    @FindBy(id = "searchbox-searchbutton")
+    WebElement searchButton;
+
+    @FindBy(css = ".section-hero-header-title-title.GLOBAL__gm2-headline-5")
+    WebElement resultHeader;
+
+    @FindBy(css = "*[class*=section-action-chip-button]")
+    List<WebElement> directionsButton;
+
+    @FindBy(css = "#sb_ifc52>.tactile-searchbox-input")
+    WebElement destinationBox;
+
     //Constructor
-    public GoogleMapsPage(WebDriver driver){
-        this.driver=driver;
+    public GoogleMapsPage(WebDriver webDriver){
+        this.driver = webDriver;
+        this.wait = new WebDriverWait(driver, 10);
         PageFactory.initElements(driver, this);
     }
 
@@ -26,9 +45,40 @@ public class GoogleMapsPage {
         driver.get(PAGE_URL);
     }
 
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    public void openWithoutCookieConsent() {
+        driver.manage().window().maximize();
+        openPage();
+        driver.manage().deleteAllCookies();
+        Cookie cookie = new Cookie("CONSENT", "YES+PT.pt-PT+V12+BX");
+        driver.manage().addCookie(cookie);
+        driver.navigate().refresh();
+    }
+
+
     public void typeInSearchBox(){
 
         searchBox.sendKeys("Dublin");
 
+    }
+
+    public void clickOnSearchButton() {
+        searchButton.click();
+    }
+
+    public void clickOnDirectionsButton() {
+        directionsButton.get(0).click();
+    }
+
+    public String resultHeaderText() {
+        wait.until(ExpectedConditions.visibilityOf(resultHeader));
+        return resultHeader.getText();
+    }
+
+    public String directionsBoxText() {
+        return destinationBox.getAttribute("aria-label");
     }
 }
--- src/test/java/test/MapsTest.java
+++ src/test/java/test/MapsTest.java
@@ -1,47 +1,30 @@
 package test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
-
-import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.GoogleMapsPage;
 
 public class MapsTest {
-    WebDriver driver = new FirefoxDriver();
+    private WebDriver driver = new FirefoxDriver();
     GoogleMapsPage mapsPage = new GoogleMapsPage(driver);
-    WebDriverWait wait = new WebDriverWait(driver, 10);
 
     @Before
     public void setup(){
-        WebDriverManager.firefoxdriver().setup();
-        driver.manage().window().maximize();
-        mapsPage.openPage();
-        // Dealing with cookie consent by setting the cookie approach
-        driver.manage().deleteAllCookies();
-        Cookie cookie = new Cookie("CONSENT", "YES+PT.pt-PT+V12+BX");
-        driver.manage().addCookie(cookie);
-        driver.navigate().refresh();
-        String driverCookie = driver.manage().getCookieNamed("CONSENT").getValue();
-        Assert.assertEquals("YES+PT.pt-PT+V12+BX", driverCookie);
+        mapsPage.openWithoutCookieConsent();
     }
 
     @Test
-    public void searchAndFindDublin() throws InterruptedException {
-        //Create object of HomePage Class
-
+    public void searchAndFindDublinDirections(){
         mapsPage.typeInSearchBox();
-        Thread.sleep(3000);
-
+        mapsPage.clickOnSearchButton();
+        Assert.assertEquals("Dublin", mapsPage.resultHeaderText());
+        mapsPage.clickOnDirectionsButton();
+        Assert.assertTrue(mapsPage.directionsBoxText().contains("Dublin"));
     }
 
     @After
     public void close(){
-        driver.close();
+        mapsPage.closeBrowser();
     }
 }
